af2e52a72244906bb799fede0ac26887f58264c5
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-16 10:06:49 -0300

    Btn Criar conta, ajuste de overflow usando o Expanded

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -6,15 +6,15 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(color: Colors.green),
-          ),
-          Expanded(
-            child: Container(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.green,
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(color: Colors.green),
+            ),
+            Container(
               padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 32),
               decoration: const BoxDecoration(
                 color: Colors.white,
@@ -70,31 +70,55 @@ class SignInScreen extends StatelessWidget {
 
                   // DIVISOR
 
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey.withAlpha(90),
-                          thickness: 2,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
                         ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15.0),
-                        child: Text('Ou'),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey.withAlpha(90),
-                          thickness: 2,
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15.0),
+                          child: Text('Ou'),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+
+                  // BTN Novo usuario
+
+                  SizedBox(
+                    height: 50.0,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                        side: const BorderSide(width: 2.0, color: Colors.green),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Criar Conta',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
