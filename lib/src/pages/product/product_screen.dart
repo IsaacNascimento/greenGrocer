0eb6769aef2960f489e84bb6de40b48724562df3
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-23 14:50:52 -0300

    Detail Screen - Argument item. Get the select item on home Screen

---
+++ lib/src/pages/product/product_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/models/item_model.dart';
+
+class ProductScreen extends StatelessWidget {
+  final ItemModel item;
+  const ProductScreen({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withAlpha(230),
+      body: Column(
+        children: [
+          Expanded(
+            child: Image.asset(item.imgUrl),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(50.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Nome - Qtde
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  // Preco
+
+                  // Descricao
+
+                  // Botao
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
