7d7e7b5318dc11291096940e75e92964811dd973
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-23 10:17:47 -0300

    CategoryTile component.

---
+++ lib/src/home/components/categoria_tile.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_color.dart';
+
+class CategoryTile extends StatelessWidget {
+  final String category;
+  final bool isSelected;
+  final VoidCallback onPressed;
+
+  const CategoryTile(
+      {super.key,
+      required this.category,
+      required this.isSelected,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            color: isSelected
+                ? CustomColors.customSwatchColor
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Text(
+            category,
+            style: TextStyle(
+                color: isSelected
+                    ? Colors.white
+                    : CustomColors.customContrastColor,
+                fontWeight: FontWeight.bold,
+                fontSize: isSelected ? 16 : 14),
+          ),
+        ),
+      ),
+    );
+  }
+}
