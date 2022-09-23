422e7c9b0f85b15d33b9d16db4a67531d15f101f
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-23 09:52:54 -0300

    Badges package and in the cart. Search field and etc

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_color.dart';
 
@@ -29,10 +30,50 @@ class HomeTab extends StatelessWidget {
           ),
         ),
         actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.shopping_cart),
-              color: CustomColors.customSwatchColor)
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0, right: 15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customContrastColor,
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(color: Colors.grey.shade400, fontSize: 14),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide:
+                      const BorderSide(width: 0, style: BorderStyle.none),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
