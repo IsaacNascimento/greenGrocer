eb60a0a8116669ae44108a343e4b475b0f4fa1ea
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-24 15:27:44 -0300

    Tela Produto. Arrow back

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -12,83 +12,117 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(230),
-      body: Column(
-        children: [
-          Expanded(
-            child: Image.asset(item.imgUrl),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(32),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(50.0),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            // Conteudo
+            Column(
+              children: [
+                Expanded(
+                  child: Image.asset(item.imgUrl),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade600,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // Nome - Qtde
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          item.itemName,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 27,
-                            fontWeight: FontWeight.bold,
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(32),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(50.0),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade600,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        // Nome - Qtde
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                item.itemName,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 27,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 30,
+                              width: 70,
+                              color: Colors.red,
+                            ),
+                          ],
+                        ),
+
+                        // Preco
+                        Text(
+                          utilsServices.priceToCurrency(item.price),
+                          style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold,
+                              color: CustomColors.customSwatchColor),
+                        ),
+
+                        // Descricao
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: SingleChildScrollView(
+                              child: Text(
+                                item.description,
+                                style: const TextStyle(height: 1.5),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        height: 30,
-                        width: 70,
-                        color: Colors.red,
-                      ),
-                    ],
-                  ),
 
-                  // Preco
-                  Text(
-                    utilsServices.priceToCurrency(item.price),
-                    style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        color: CustomColors.customSwatchColor),
-                  ),
-
-                  // Descricao
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10.0),
-                      child: SingleChildScrollView(
-                        child: Text(
-                          item.description,
-                          style: const TextStyle(height: 1.5),
+                        // Botao
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            onPressed: () {},
+                            label: const Text(
+                              'Add no carrinho',
+                              style: TextStyle(
+                                  fontSize: 18.0, fontWeight: FontWeight.bold),
+                            ),
+                            icon: const Icon(
+                              Icons.shopping_cart_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
+                ),
+              ],
+            ),
 
-                  // Botao
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Botao'),
-                  ),
-                ],
+            Positioned(
+              left: 20,
+              top: 10,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
